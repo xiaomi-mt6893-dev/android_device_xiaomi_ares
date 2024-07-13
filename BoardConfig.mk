36125302bde2e2ad519b277e98e2a016cbf4dab2
ZiadTamer <ziadtamer150@gmail.com>
2024-07-13 05:32:06 +0300

    ares: Configure Virtual A/B OTAs
    Change-Id: I47b98a2be724561e775db9e78e65ec7322017daa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,19 @@
 
 DEVICE_PATH := device/xiaomi/ares
 
+# A/B
+AB_OTA_UPDATER := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    system \
+    system_ext \
+    product \
+    vendor \
+    vbmeta
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
